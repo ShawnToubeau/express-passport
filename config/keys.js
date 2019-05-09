440e43172d651088cf71274097b8fbca11ad3cb0
Shawn Toubeau <shawntoubeau@gmail.com>
2019-05-09 09:10:13 -0400

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MongoURI:
+    "mongodb+srv://shawntoubeau:nolongerbutter@cluster0-obghw.mongodb.net/test?retryWrites=true"
+};
---
+++ config/passport.js
@@ -0,0 +1,44 @@
+const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+// Load user model
+const User = require("../models/User");
+
+module.exports = passport => {
+  passport.use(
+    new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
+      // Match User
+      User.findOne({ email: email })
+        .then(user => {
+          if (!user) {
+            return done(null, false, {
+              message: "That email is not registered"
+            });
+          }
+
+          // Match Password
+          bcrypt.compare(password, user.password, (err, isMatch) => {
+            if (err) throw err;
+
+            if (isMatch) {
+              return done(null, user);
+            } else {
+              return done(null, false, { message: "Password is incorrect" });
+            }
+          });
+        })
+        .catch(err => console.log(err));
+    })
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+      done(err, user);
+    });
+  });
+};
